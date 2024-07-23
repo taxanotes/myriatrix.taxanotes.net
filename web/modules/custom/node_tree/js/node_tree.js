76cb05ef6e3c339d735ee15333814487a2f32e36
Rob Davis <login@therobyouknow.com>
2024-07-23 17:29:51 +0100

    clean up of js code - formatting

--- web/modules/custom/node_tree/js/node_tree.js
+++ web/modules/custom/node_tree/js/node_tree.js
@@ -1,65 +1,33 @@
 (function ($, Drupal) {
     Drupal.behaviors.node_tree = {
       attach: function (context, settings) {
-
       // expecting to remove this if testing confirms everything still works
-
     }
   };
 
-  
-
-// data-portal/node/12345
+  // data-portal/node/12345
   Drupal.behaviors.node_tree_open_tree_at = {
     attach: function (context, settings) {
+      var nodeId = getNodeIdFromUrl();
 
-        var nodeId = getNodeIdFromUrl();
-
-
-
-
-
-        if ( nodeId !== undefined ) {
-          var newdiv2 =  $(document.createElement('div')).attr('id', 'node_treetreeloading');
-          newdiv2.text('Loading tree expanded to selected taxon...');
-          $('#block-node_treehierarchicaltaxontree-2').append(newdiv2);
-          $.get('/node_tree/api/getExpandedTreePathToNode/' + nodeId, getExpandedTreePathToNodeCallback );
-        }
-        else {
-          
-          var newdiv2 =  $(document.createElement('div')).attr('id', 'node_treetree');
-          newdiv2.attr('data-url', '/node_tree/api/getImmediateChildrenOfParentnode_tree/');
-          $('#block-node_treehierarchicaltaxontree-2').append(newdiv2);
-          
-
-      
-          /*
-          var node_treetree = $('#node_treetree').tree({
-            data: response,
-            autoOpen: true,
-            saveState: true,
-          });
-          */
-
-          
-          var node_treetree = $('#node_treetree').tree({
-          });
-      
-          node_treeTreeEvents( node_treetree );
-          
-          
-        }
-        /*
-        else {
-          $.get('/node_tree/api/getExpandedTreePathToNode', getExpandedTreePathToNodeCallback );
-          //$.get('/node_tree/api/getExpandedTreePathToRoot', getExpandedTreePathToNodeCallback );
-        }
-        */
-
+      if ( nodeId !== undefined ) {
+        var newdiv2 =  $(document.createElement('div')).attr('id', 'node_treetreeloading');
+        newdiv2.text('Loading tree expanded to selected taxon...');
+        $('#block-node_treehierarchicaltaxontree-2').append(newdiv2);
+        $.get('/node_tree/api/getExpandedTreePathToNode/' + nodeId, getExpandedTreePathToNodeCallback );
       }
+      else {
+        var newdiv2 =  $(document.createElement('div')).attr('id', 'node_treetree');
+        newdiv2.attr('data-url', '/node_tree/api/getImmediateChildrenOfParentnode_tree/');
+        $('#block-node_treehierarchicaltaxontree-2').append(newdiv2);
+          
+        var node_treetree = $('#node_treetree').tree({
+        });
+      
+        node_treeTreeEvents( node_treetree );
+      }
+    }
   };
-
-
 
   var getImmediateChildrenOfParentnode_treeCallback = function(response) {
 
@@ -74,12 +42,8 @@
    * @param {*} response 
    */
   var getExpandedTreePathToNodeCallback = function(response) {
-
     // https://github.com/mbraak/jqTree/issues/301#issuecomment-133307420
-
-
     $('#node_treetreeloading').remove();
-
 
     var newdiv2 =  $(document.createElement('div')).attr('id', 'node_treetree');
     newdiv2.attr('data-url', '/node_tree/api/getImmediateChildrenOfParentnode_tree/');
@@ -105,50 +69,44 @@
    */
   var node_treeTreeEvents = function( node_treetree ) {
     node_treetree.on(
-        'tree.select',
-        function(event) {
-          if (event.node) {
-            // https://mbraak.github.io/jqTree/#event-tree-select
+      'tree.select',
+      function(event) {
+        if (event.node) {
+          // https://mbraak.github.io/jqTree/#event-tree-select
 
-            // change browser address bar URL to link directly to the node, for saving in bookmarks, history, sharing
-            // https://stackoverflow.com/a/3503206/227926
-            var node = event.node;
-            window.history.pushState('permalink-to-' + node.id, node.name, '/data-portal/node/' + node.id );
+          // change browser address bar URL to link directly to the node, for saving in bookmarks, history, sharing
+          // https://stackoverflow.com/a/3503206/227926
+          var node = event.node;
+          window.history.pushState('permalink-to-' + node.id, node.name, '/data-portal/node/' + node.id );
 
+          // https://mbraak.github.io/jqTree/#functions-loaddata
+          //
 
+          //$.get('/node_tree/api/getImmediateChildrenOfParentnode_tree/', { node: node.id }, getImmediateChildrenOfParentnode_treeCallback );
 
-            // https://mbraak.github.io/jqTree/#functions-loaddata
-            //
+          // get the node_tree node content for display on the right hand side
 
-
-            //$.get('/node_tree/api/getImmediateChildrenOfParentnode_tree/', { node: node.id }, getImmediateChildrenOfParentnode_treeCallback );
-
-            // get the node_tree node content for display on the right hand side
-
-            $.get('/node_tree/api/getnode_treeContent/' + node.id, node_treeContentCallback);  
-
-
+          $.get('/node_tree/api/getnode_treeContent/' + node.id, node_treeContentCallback);  
             
-            taxon = node.name; // node.name is the node title field
+          taxon = node.name; // node.name is the node title field
 
-            if ( taxon !== '' ) {
-              var params = {
-                'taxon': taxon,
-              }
+          if ( taxon !== '' ) {
+            var params = {
+              'taxon': taxon,
+            }
 
-              console.log( '[' + taxon + ']' );
+            console.log( '[' + taxon + ']' );
 
-              $.get('/node_tree/api/noticeProgressnode_treeata', callbackNoticeProgressnode_treeata );
+            $.get('/node_tree/api/noticeProgressnode_treeata', callbackNoticeProgressnode_treeata );
 
-
-              $.get('/node_tree/externalapi', params, getnode_treeataCallback);  // TO DO  - to remove 
-
+            $.get('/node_tree/externalapi', params, getnode_treeataCallback);  // TO DO  - to remove 
           }
           else {
-              // event.node is null
-              // a node was deselected
-              // e.previous_node contains the deselected node
+            // event.node is null
+            // a node was deselected
+            // e.previous_node contains the deselected node
           }
+
           event.stopPropagation();
           return false;
         }
@@ -158,7 +116,6 @@
     // need event for /node_tree/api/getImmediateChildrenOfParentnode_tree/
   }
 
-  
   /**
    * 
    * @param {*} response 
@@ -170,14 +127,12 @@
     $('.node_treecontentdisplay').html(response.data);
   }    
 
-
   /**
    * 
    * @param {*} response 
    */
   var getnode_treeataCallback = function(response) {
-    if ( Object.keys(response.data).length > 0 )
-    {
+    if ( Object.keys(response.data).length > 0 ) {
 
       console.dir(response.data);
 
@@ -185,7 +140,6 @@
 
       // just relay the response.data content onto the next api call, no need to package or format
       $.post( '/node_tree/api/getnode_treeataOutput', response.data, getnode_treeataDisplayCallback, 'text' );
-
     }
   }   
 
@@ -209,7 +163,6 @@
    * @param {*} response 
    */
   var callbackNoticeProgressnode_treeata = function(response) {
-
 
     // not required, don't know why
     //var responseAsObject = JSON.parse( response );
